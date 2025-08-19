dc2c6f153cc2c1c4eb1f0b9c95cfa3cb5c55296c
Pankaj Yadav <pankajyadav00027@gmail.com>
2025-08-19 18:42:16 +0530

    library as spring boot starter

---
+++ src/main/java/com/common/config/PropertiesConfig.java
@@ -0,0 +1,12 @@
+package com.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+
+@Configuration
+@PropertySources({
+        @PropertySource("classpath:constant.properties")
+})
+public class PropertiesConfig {
+}
---
+++ src/main/java/com/common/util/Utils.java
@@ -0,0 +1,7 @@
+package com.common.util;
+
+public class Utils {
+//    public static String getMessage(String property){
+//
+//    }
+}
