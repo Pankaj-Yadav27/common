3a7a4948bcf34d96e0ff3b33bad87fce1026c96f
Pankaj Yadav <pankajyadav00027@gmail.com>
2025-08-21 15:28:38 +0530

    env files renamed

--- src/main/java/com/common/config/PropertiesConfig.java
+++ src/main/java/com/common/config/PropertiesConfig.java
@@ -6,7 +6,9 @@ import org.springframework.context.annotation.PropertySources;
 
 @Configuration
 @PropertySources({
-        @PropertySource("classpath:constant.properties")
+        @PropertySource("classpath:constant.properties"),
+        @PropertySource("classpath:common.properties"),
+        @PropertySource(value="classpath:common-${spring.profiles.active}.properties",ignoreResourceNotFound = true)
 })
 public class PropertiesConfig {
 }
